5dbf125ce74ab3e53e48f192068f5918706e3f5e
Yeboah William <97909844+Billboah@users.noreply.github.com>
2024-06-03 00:37:57 +0000

    commit

--- src/pages/layout/index.tsx
+++ src/pages/layout/index.tsx
@@ -7,10 +7,13 @@ import Image from "next/image";
 import axios from "axios";
 import { ClipLoading } from "@/config/appLoading";
 import Trip from "@/Components/Trip";
+import { FaSignOutAlt, FaUser } from "react-icons/fa";
+import { FcAbout, FcBusinessman } from "react-icons/fc";
 
 interface UserData {
   name: string;
   photoUrl: string;
+  email: string;
 }
 
 const ComponentLayout = ({
@@ -21,12 +24,13 @@ const ComponentLayout = ({
   pageName: any;
 }) => {
   const { data: session, status } = useSession();
-  const router = useRouter();
+  const { push } = useRouter();
   const [user, setUser] = useState<UserData | null>(null);
   const [history, setHistory] = useState(false);
   const [loading, setLoading] = useState(false);
   const [apiError, setApiError] = useState("");
   const [trip, setTrip] = useState<any>("");
+  const [profileMenu, setProfileMenu] = useState(false);
   const ref = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
@@ -36,19 +40,17 @@ const ComponentLayout = ({
       setUser({
         name: session.user.name,
         photoUrl: session.user.image,
+        email: session.user.email,
       });
-      clearTimeout(timeoutId);
-    } else {
+    } /*else {
       setUser(null);
       if (router.pathname !== "/login") {
         timeoutId = setTimeout(() => {
           router.push("/login");
         }, 30000);
-      }
-    }
-
-    return () => clearTimeout(timeoutId);
-  }, [session, router]);
+      
+    }*/
+  }, [session, push]);
 
   const displayHistory = async () => {
     setHistory(true);
@@ -94,18 +96,25 @@ const ComponentLayout = ({
     };
   }, []);
 
+  useEffect(() => {
+    const handleClickOutside = (e: MouseEvent) => {
+      if (ref.current && !ref.current.contains(e.target as Node)) {
+        setProfileMenu(false);
+      }
+    };
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
   return (
     <div className="h-screen w-full">
       <div className="h-full w-full min-w-[250px] bg-white flex flex-col">
         {pageName !== "ConfirmPage" && (
           <nav className="h-[70px] px-4 flex items-center justify-between bg-black text-white">
-            <Link
-              href={`${pageName === "LoginPage" ? "#" : "/"}`}
-              className={`${
-                pageName === "LoginPage" ? "cursor-default" : "cursor-pointer"
-              }`}
-              passHref
-            >
+            <Link href="/" className="cursor-pointer" passHref>
               <Image
                 src="/images/uber-logo.png"
                 alt="uber logo"
@@ -114,7 +123,7 @@ const ComponentLayout = ({
               />
             </Link>
             <div className="flex items-center">
-              {pageName === "HomePage" && (
+              {pageName === "HomePage" && user && (
                 <div className="flex items-center">
                   <button
                     className="mr-4 font-semibold"
@@ -122,24 +131,79 @@ const ComponentLayout = ({
                   >
                     History
                   </button>
-                  <div className="mr-2 w-fit text-sm">{user && user.name}</div>
                 </div>
               )}
-              {user && (
-                <img
-                  className={`h-10 w-10 ${
-                    pageName === "HomePage"
-                      ? "cursor-pointer"
-                      : "cursor-default"
-                  } rounded-full border border-gray-100 p-px`}
-                  src={user.photoUrl}
-                  onClick={() =>
-                    pageName === "HomePage" &&
-                    signOut({ redirect: false, callbackUrl: "/login" })
-                  }
-                  alt="user image"
-                  title={`${pageName === "HomePage" ? "Sign out" : ""}`}
-                />
+              {user ? (
+                <div className="flex flex-col">
+                  <div className="relative " ref={ref}>
+                    <button onClick={() => setProfileMenu((prev) => !prev)}>
+                      {user.photoUrl ? (
+                        <img
+                          src={user.photoUrl}
+                          alt=""
+                          className={`h-10 w-10 cursor-pointer rounded-full border border-gray-100 p-px`}
+                          title="User"
+                        />
+                      ) : (
+                        <FcBusinessman
+                          size={50}
+                          className="w-10 rounded-full"
+                        />
+                      )}
+                    </button>
+                    {profileMenu && (
+                      <div className="bg-gray-200 absolute top-[125%] right-0 z-20 flex flex-col rounded border  border-gray-400 shadow-lg w-[200px] min-w-fit h-[200px] before:block before:h-[20px] before:w-[20px] before:rotate-45 before:absolute before:top-[-11px] before:right-[5%]  before:bg-gray-200 before:border-t before:border-l before:border-gray-400">
+                        <div className="flex p-[15px] border-b border-b-gray-400">
+                          <div className="mr-[10px]">
+                            {user.photoUrl ? (
+                              <img
+                                src={user.photoUrl}
+                                alt=""
+                                width={60}
+                                height={60}
+                                className=" rounded-full"
+                              />
+                            ) : (
+                              <FaUser size={50} className="w-10 rounded-full" />
+                            )}
+                          </div>
+                          <div className="flex flex-col justify-between py-[5px] text-xs text-gray-500 ">
+                            <div>{user.name}</div>
+                            <div>{user.email}</div>
+                          </div>
+                        </div>
+                        <div className=" text-lg text-gray-600">
+                          <Link
+                            href="#"
+                            className="flex items-center p-[7px] hover:bg-gray-300 w-full"
+                          >
+                            <FaUser size={16} color="gray" />
+                            <span className="ml-[6px]">Profile</span>
+                          </Link>
+                          <button
+                            onClick={() => {
+                              signOut();
+                              window.location.reload();
+                            }}
+                            className="flex items-center p-[7px] hover:bg-gray-300 w-full"
+                          >
+                            <FaSignOutAlt size={16} color="gray" />
+                            <span className="ml-[6px]">Logout</span>
+                          </button>
+                        </div>
+                      </div>
+                    )}
+                  </div>
+                </div>
+              ) : (
+                <div>
+                  <button
+                    onClick={() => push(`/login`)}
+                    className="h-[35px] w-[120px] bg-gray-500 hover:bg-gray-300 hover:text-black mx-[10px] p-[3px] rounded   border-1-inherit shadow-md active:shadow-inner "
+                  >
+                    SignIn
+                  </button>
+                </div>
               )}
             </div>
           </nav>
